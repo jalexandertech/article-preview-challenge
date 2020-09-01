2c29697e25c27194e1445dbc4f4345bcc4b10435
webluxdesigns <webluxdesigns@gmail.com>
2020-08-31 18:41:41 -0700

    Finish project

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
-$(".share-icon").click(function() {
-    $(".social").toggle();
+    $('.share-icon').on('click', () => {
+        $('.social').toggle();
     });
 });
+
+
+
+
+// $(document).ready(function(){
+// $(".share-icon").click(function() {
+//     $(".social").toggle();
+//     });
+// });
+
+
+// function openClose(){
+//     let container = document.querySelector('.share-icon');
+//     let x = document.querySelector('.social')
+//     if (container.style.display === "none") {
+//         container.style.display = "display";
+//     }
+
+//     else {
+//         container.style.display ="none"
+//     }
+
+
